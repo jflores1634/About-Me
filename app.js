215696c1b619909cfb43f7176348d95536c721ab
Jose Flores <jose.flores1634@gmail.com>
2017-12-28 20:00:34 -0800

    re submit

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 'use strict'
 
 var shoes = prompt('how many shoes do you have?');
-console.log('num of shoes', shoes);
-
-var word = 'something'
+console.log('num of shoes:', shoes);
 
 var socks = prompt('how many socks do you have?');
 console.log('num of socks:', socks);
@@ -12,9 +10,15 @@ if (shoes === '3' || shoes === '4') {
   alert('yay! you have a lot of shoes!');
 }
 
-if (shoes === '3' && socks.toLowerCase() === 'none') {
-  alert('well alrighty then.');
+var shirts = prompt('how many shirts do you have?');
+console.log('num of shirts:', shirts);
+
+if (shirts === '5' || shirts === '10') {
+  alert('thats a lot of shirts!');
 }
 
-var difficulty = prompt('what level of difficulty are yo used to?');
-console.log('my users answer', answer);
+var pants = prompt('how many pants do you have?');
+console.log('num of pants:', pants);
+
+var blankets =  prompt('how many blankets do you have?');
+console.log('num of blankets', blankets);
